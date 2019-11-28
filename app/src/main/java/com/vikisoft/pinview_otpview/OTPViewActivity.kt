4b7f23aa4a03f2d795769db55a79c49198ce142a
crisBoi <pranaycris@gmail.com>
2019-11-28 12:37:16 +0530

    entering otp logic added

--- app/src/main/java/com/vikisoft/pinview_otpview/OTPViewActivity.kt
+++ app/src/main/java/com/vikisoft/pinview_otpview/OTPViewActivity.kt
@@ -6,7 +6,6 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.view.KeyEvent
 import android.widget.EditText
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class OTPViewActivity : AppCompatActivity() {
@@ -24,41 +23,37 @@ class OTPViewActivity : AppCompatActivity() {
     }
 
     private fun setDisable() {
-
-        fun disableEditText(editText: EditText?) {
-            if (editText != null) {
-                editText.isEnabled = false
-            }
-
-        }
-
         disableEditText(otp_et2)
         disableEditText(otp_et3)
         disableEditText(otp_et4)
         disableEditText(otp_et5)
         disableEditText(otp_et6)
+    }
+
+    fun disableEditText(editText: EditText?) {
+        if (editText != null) {
+            editText.isEnabled = false
+        }
 
     }
     
-    private fun onOtpEntered(editText: EditText) {
+    private fun onOtpEntered(editText: EditText?) {
 
-        
-        enableEditText(editText)
 
-        editText.addTextChangedListener(object : TextWatcher {
+        editText!!.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(p0: Editable?) {
                 if (p0.toString().isEmpty()) {
                     editText.setOnKeyListener { view, i, keyEvent ->
-                        if (keyEvent.keyCode == KeyEvent.KEYCODE_DEL) {
+                       /* if (keyEvent.keyCode == KeyEvent.KEYCODE_DEL) {
                             when(editText) {
-                                otp_et2 -> onOtpEntered(otp_et1)
-                                otp_et3 -> onOtpEntered(otp_et2)
-                                otp_et4 -> onOtpEntered(otp_et3)
-                                otp_et5 -> onOtpEntered(otp_et4)
-                                otp_et6 -> onOtpEntered(otp_et5)
+                                otp_et2 -> enablePrevious(otp_et2, otp_et1)
+                                otp_et3 -> enablePrevious(otp_et3, otp_et2)
+                                otp_et4 -> enablePrevious(otp_et4, otp_et3)
+                                otp_et5 -> enablePrevious(otp_et5, otp_et4)
+                                otp_et6 -> enablePrevious(otp_et6, otp_et5)
 
                             }
-                        }
+                        }*/
                         true
                     }
                 }
@@ -70,19 +65,35 @@ class OTPViewActivity : AppCompatActivity() {
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 when(editText) {
-                    otp_et1 -> onOtpEntered(otp_et2)
-                    otp_et2 -> onOtpEntered(otp_et3)
-                    otp_et3 -> onOtpEntered(otp_et4)
-                    otp_et4 -> onOtpEntered(otp_et5)
-                    otp_et5 -> onOtpEntered(otp_et6)
+                    otp_et1 -> disablePrevious(otp_et1, otp_et2)
+                    otp_et2 -> disablePrevious(otp_et2, otp_et3)
+                    otp_et3 -> disablePrevious(otp_et3, otp_et4)
+                    otp_et4 -> disablePrevious(otp_et4, otp_et5)
+                    otp_et5 -> disablePrevious(otp_et5, otp_et6)
 
                 }
             }
         })
     }
 
-    private fun enableEditText(editText: EditText) {
-        editText.isEnabled = true
-        editText.requestFocus()
+
+    private fun enablePrevious(currentEditText: EditText, previousEditText : EditText?) {
+        disableEditText(currentEditText)
+        if (previousEditText != null) {
+            previousEditText.isEnabled = true
+            previousEditText.requestFocus()
+        }
+
+    }
+
+    private fun disablePrevious(currentEditText: EditText, nextEditText : EditText?) {
+
+        onOtpEntered(nextEditText)
+
+        disableEditText(currentEditText)
+        if (nextEditText != null) {
+            nextEditText.isEnabled = true
+            nextEditText.requestFocus()
+        }
     }
 }
