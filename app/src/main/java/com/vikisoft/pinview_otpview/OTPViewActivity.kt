1efa0dc911dd74d5486148ff9f4901ebbd30422a
crisBoi <pranaycris@gmail.com>
2019-11-28 14:54:09 +0530

    text disapperance bug fixed

--- app/src/main/java/com/vikisoft/pinview_otpview/OTPViewActivity.kt
+++ app/src/main/java/com/vikisoft/pinview_otpview/OTPViewActivity.kt
@@ -1,11 +1,14 @@
 package com.vikisoft.pinview_otpview
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.KeyEvent
+import android.view.View
 import android.widget.EditText
+import androidx.annotation.RequiresApi
 import kotlinx.android.synthetic.main.activity_main.*
 
 class OTPViewActivity : AppCompatActivity() {
@@ -37,43 +40,42 @@ class OTPViewActivity : AppCompatActivity() {
 
     }
     
+
     private fun onOtpEntered(editText: EditText?) {
 
+        editText!!.setOnKeyListener { view, i, keyEvent ->
+           /* if (editText.text.isEmpty())
+            {*/
+                if (keyEvent.keyCode == KeyEvent.KEYCODE_DEL) {
+                    if (editText.text.isEmpty())
+                    when(editText) {
+                        otp_et2 -> enablePrevious(otp_et2, otp_et1)
+                        otp_et3 -> enablePrevious(otp_et3, otp_et2)
+                        otp_et4 -> enablePrevious(otp_et4, otp_et3)
+                        otp_et5 -> enablePrevious(otp_et5, otp_et4)
+                        otp_et6 -> enablePrevious(otp_et6, otp_et5)
 
-        editText!!.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(p0: Editable?) {
-                if (p0.toString().isEmpty()) {
-                    editText.setOnKeyListener { view, i, keyEvent ->
-                       /* if (keyEvent.keyCode == KeyEvent.KEYCODE_DEL) {
-                            when(editText) {
-                                otp_et2 -> enablePrevious(otp_et2, otp_et1)
-                                otp_et3 -> enablePrevious(otp_et3, otp_et2)
-                                otp_et4 -> enablePrevious(otp_et4, otp_et3)
-                                otp_et5 -> enablePrevious(otp_et5, otp_et4)
-                                otp_et6 -> enablePrevious(otp_et6, otp_et5)
-
-                            }
-                        }*/
-                        true
                     }
                 }
+
+                else {
+                    if(!editText.text.isEmpty())
+                    when(editText) {
+                        otp_et1 -> disablePrevious(otp_et1, otp_et2)
+                        otp_et2 -> disablePrevious(otp_et2, otp_et3)
+                        otp_et3 -> disablePrevious(otp_et3, otp_et4)
+                        otp_et4 -> disablePrevious(otp_et4, otp_et5)
+                        otp_et5 -> disablePrevious(otp_et5, otp_et6)
+
+                    }
+//                }
             }
+            false
+        }
 
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
-            }
 
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                when(editText) {
-                    otp_et1 -> disablePrevious(otp_et1, otp_et2)
-                    otp_et2 -> disablePrevious(otp_et2, otp_et3)
-                    otp_et3 -> disablePrevious(otp_et3, otp_et4)
-                    otp_et4 -> disablePrevious(otp_et4, otp_et5)
-                    otp_et5 -> disablePrevious(otp_et5, otp_et6)
 
-                }
-            }
-        })
     }
 
 
@@ -82,6 +84,7 @@ class OTPViewActivity : AppCompatActivity() {
         if (previousEditText != null) {
             previousEditText.isEnabled = true
             previousEditText.requestFocus()
+            previousEditText.setText("")
         }
 
     }
